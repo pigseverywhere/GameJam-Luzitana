6ef76c92029dff9debf7e0b7a6421a3e52695443
SulivanSergio <sulivansergio22@gmail.com>
2022-10-15 11:47:12 -0300

    Menu

---
+++ GameJam Luzitana/Assets/Scripts/MudarScene.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MudarScene : MonoBehaviour
+{
+    public string nome;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+    public void MudarCena()
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(nome);
+
+    }
+
+}
